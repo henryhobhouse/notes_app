cb51d3d80e8a162a949b4b2ae3e336c4c5b7f87a
Henry Hobhouse <henryhobhouse@gmail.com>
2017-05-25 13:27:48 +0100

    refactored app

--- app/public/js/notes.js
+++ app/public/js/notes.js
@@ -7,25 +7,26 @@ function Notes() {
   this.list = [];
 };
 
-Notes.prototype.addNote = function(string){
-  string = document.getElementById('box').value
+Notes.prototype.addNote = function(){
+  var string = document.getElementById('box').value
   this.list.push(string)
-    var newLink = document.createElement("a")
-    newLink.setAttribute("id", `a${this.list.length}`);
-    console.log(this.list.length);
-    console.log(this.list[this.list.length]);
-    newLink.setAttribute("href", `#a${this.list.length}all`);
-    var displayNoteLink = document.getElementById('displayNoteLink')
-    displayNoteLink.append(newLink)
-    document.getElementById(`a${this.list.length}`).innerHTML = this.list[this.list.length-1].substring(0,20) + '<br>'
+  this.showLink();
+  this.showNote();
+};
 
-    var newNote = document.createElement("p")
-    newNote.setAttribute("id", `a${this.list.length}all`);
-    var displayNote = document.getElementById('displayNote')
-    displayNote.append(newNote)
-    document.getElementById(`a${this.list.length}all`).innerHTML = this.list[this.list.length-1] + '<br>'
-}
+Notes.prototype.showLink = function() {
+  var newLink = document.createElement("a");
+  newLink.setAttribute("id", `a${this.list.length}`);
+  newLink.setAttribute("href", `#a${this.list.length}all`);
+  var displayNoteLink = document.getElementById('displayNoteLink');
+  displayNoteLink.append(newLink);
+  document.getElementById(`a${this.list.length}`).innerHTML = this.list[this.list.length-1].substring(0,20) + '<br>';
+};
 
-Notes.prototype.addLink = function() {
-  
-}
+Notes.prototype.showNote = function() {
+  var newNote = document.createElement("p");
+  newNote.setAttribute("id", `a${this.list.length}all`);
+  var displayNote = document.getElementById('displayNote');
+  displayNote.append(newNote);
+  document.getElementById(`a${this.list.length}all`).innerHTML = this.list[this.list.length-1] + '<br>';
+};
