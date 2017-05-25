ba040bac575bcac6d32eb3f1cc0eb4e6c5a19683
Henry Hobhouse <henryhobhouse@gmail.com>
2017-05-25 13:17:24 +0100

    App now displays note link and full notes which connect via hash href

--- app/public/js/notes.js
+++ app/public/js/notes.js
@@ -10,12 +10,22 @@ function Notes() {
 Notes.prototype.addNote = function(string){
   string = document.getElementById('box').value
   this.list.push(string)
-  for (var i = 0; i < this.list.length; i++) {
     var newLink = document.createElement("a")
-    newLink.setAttribute("id", `a${i}`);
-    newLink.setAttribute("href", "");
+    newLink.setAttribute("id", `a${this.list.length}`);
+    console.log(this.list.length);
+    console.log(this.list[this.list.length]);
+    newLink.setAttribute("href", `#a${this.list.length}all`);
     var displayNoteLink = document.getElementById('displayNoteLink')
     displayNoteLink.append(newLink)
-    document.getElementById(`a${i}`).innerHTML = this.list[i].substring(0,20) + '<br>'
-  }
+    document.getElementById(`a${this.list.length}`).innerHTML = this.list[this.list.length-1].substring(0,20) + '<br>'
+
+    var newNote = document.createElement("p")
+    newNote.setAttribute("id", `a${this.list.length}all`);
+    var displayNote = document.getElementById('displayNote')
+    displayNote.append(newNote)
+    document.getElementById(`a${this.list.length}all`).innerHTML = this.list[this.list.length-1] + '<br>'
+}
+
+Notes.prototype.addLink = function() {
+  
 }
